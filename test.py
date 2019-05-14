8863d9d1d9c18c9f4a877b261bf09d1759a21b4b
Paul Schalkwijk <pschalkwijk@Pauls-MacBook-Pro.local>
2019-05-14 16:27:19 +0200

    changed filenames

--- test.py
+++ test.py
@@ -20,14 +20,14 @@ mTA_CL2 = MatlabTA(CL2_M_20)
 
 from ControlLoop import ControlLoop
 loops = []
-for i in range(2):
+for i in range(1):
     loops.append(ControlLoop(mTA_CL1, f'cl{i}'))
 
-for i in range(2):
+for i in range(1):
     loops.append(ControlLoop(mTA_CL2, f'cl{2+i}'))
 #
 from Network import Network
-net = Network(1, 1) # the network is 0.005/0.001 seconds active
+net = Network(1, 5) # the network is 0.005/0.001 seconds active
 loops.append(net)
 
 # NOTE: To be more specific, this could be described with a NTGA.
@@ -36,21 +36,22 @@ from ta import NTA
 ntga = NTA(*loops)
 
 from datetime import datetime
+strategy_name = shortuuid.uuid()[:6]
 filename = f'ntga_{datetime.now()}'
-with open(f'xml/{filename}.xml', 'w') as file:
+with open(f'xml/{strategy_name}.xml', 'w') as file:
     file.write(ntga.to_xml())
 
-strategy_name = shortuuid.uuid()[:6]
-with open(f'queries/{filename}.q', 'w') as file:
+
+with open(f'queries/{strategy_name}.q', 'w') as file:
     file.write(f"strategy {strategy_name} = control: A[] not ({net.name}{net.index}.Bad)")
 
 
-arg_list = [VERIFYTA, '-u','-s','--print-strategies','strat', f'xml/{filename}.xml', f'queries/{filename}.q']
+arg_list = [VERIFYTA, '-u','-s','--generate-strategy','2','--print-strategies','strat', f'xml/{strategy_name}.xml', f'queries/{strategy_name}.q']
 print(' '.join(arg_list))
 verify_ta = subprocess.run(arg_list, stdout=subprocess.PIPE)
 print(f"strategy written to strat/{strategy_name}")
 result = verify_ta.stdout.decode('utf-8')
-with open(f'results/{filename}.txt', 'w') as file:
+with open(f'results/{strategy_name}.txt', 'w') as file:
     file.write(result)
 print(result)
-print(f"strategy written to results/{filename}.txt")
+print(f"strategy written to results/{strategy_name}.txt")
